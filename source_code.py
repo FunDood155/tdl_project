81dfaa079fc8895cf3e3790af6818007da8c207b
FunDood155 <dkaarthik2007@gmail.com>
2026-03-06 00:10:44 +0530

    day 1

---
+++ source_code.py
@@ -0,0 +1,39 @@
+import mysql.connector 
+
+con=mysql.connector.connect(host="localhost",user="root",passwd="root155",database="tdldb")
+cursor=con.cursor()
+
+cursor.execute("use tdldb;")
+
+#table creation
+a="create table if not exists tdl_table (num int auto_increment primary key , work varchar(99), date timestamp default current_timestamp,status varchar(99));"
+cursor.execute(a)
+
+
+#add_activity
+a="insert into tdl_table (work,status) values (%s,%s);"
+cursor.execute(a,("gym","no"))
+con.commit()
+
+#display_activity
+a="select * from tdl_table;"
+cursor.execute(a)
+z=cursor.fetchall()
+print(z)
+
+
+
+#saving changes...
+con.commit()
+cursor.close()
+
+
+def add_activity():
+    pass
+def remove_activity():
+    pass
+def display_activity():
+    pass
+def clear():
+    #clear table
+    cursor.execute("drop table tdl_table")
