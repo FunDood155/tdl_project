41ce00acc91e9ce4b9b13fc21c43263b4f5f8301
FunDood155 <dkaarthik2007@gmail.com>
2026-03-07 22:54:33 +0530

    full code done

--- source_code.py
+++ source_code.py
@@ -9,31 +9,53 @@ cursor.execute("use tdldb;")
 a="create table if not exists tdl_table (num int auto_increment primary key , work varchar(99), date timestamp default current_timestamp,status varchar(99));"
 cursor.execute(a)
 
+def add_activity():
+    #add_activity
+    a="insert into tdl_table (work,status) values (%s,%s);"
+    t=input("Enter activity to add : ")
+    cursor.execute(a,(t,"no"))
+    con.commit()
 
-#add_activity
-a="insert into tdl_table (work,status) values (%s,%s);"
-cursor.execute(a,("gym","no"))
-con.commit()
+def remove_activity():
+    #remove an activity
+    a="delete from tdl_table where num= (%s);"
+    t=int(input("Enter activity to remove(num) : "))
+    cursor.execute(a,(t,))
+    con.commit()
 
-#display_activity
-a="select * from tdl_table;"
-cursor.execute(a)
-z=cursor.fetchall()
-print(z)
+def display_activity():
+    #display_activity
+    a="select * from tdl_table;"
+    cursor.execute(a)
+    z=cursor.fetchall()
+    for i in z:
+        print(i)
+    
+def table_clear():
+    #clear table
+    cursor.execute("truncate table tdl_table")
 
+    ###     MAIN    ###
 
-
+ch=1
+print("1.Add, 2.Remove, 3.Display, 4.Clear")
+while(ch in [1,2,3,4]):
+    ch=int(input("Enter ur choice : "))
+    match ch:
+        case 1:
+            add_activity()
+        case 2:
+            remove_activity()
+        case 3:
+            display_activity()
+        case 4:
+            table_clear()
+        case _:
+            print("Successfully EXITED...")
+            break
+    print("\n* * *  over  * * *\n")
 #saving changes...
 con.commit()
 cursor.close()
 
 
-def add_activity():
-    pass
-def remove_activity():
-    pass
-def display_activity():
-    pass
-def table_clear():
-    #clear table
-    cursor.execute("drop table tdl_table")
