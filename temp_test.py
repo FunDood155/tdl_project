1a6062861df79cd189fa6740d8c1cc14a8f228a8
FunDood155 <dkaarthik2007@gmail.com>
2026-03-10 22:01:53 +0530

    ignore

--- temp_test.py
+++ temp_test.py
@@ -1,16 +1,30 @@
-a=[1,2,5,9,25,8]
-# m=l[0]
-# s=0
-# for i in l:
-#     if(m<i):
-#         m=i
-#     s=s+i
-# print("Max is :",m)
-# print("sum is : ",s)
+class Solution:
+    def addTwoNumbers(self, l1, l2):
 
+        a=[]
+        b=[]
 
-b=[]
-b=a.copy()
-print(b)
-a=[3,4]
-print(b)
+        while l1:
+            a.append(l1.val)
+            l1=l1.next
+
+        while l2:
+            b.append(l2.val)
+            l2=l2.next
+
+        a.reverse()
+        b.reverse()
+
+        n=int("".join(map(str,a)))
+        m=int("".join(map(str,b)))
+
+        s=str(n+m)[::-1]
+
+        dummy=ListNode()
+        cur=dummy
+
+        for i in s:
+            cur.next=ListNode(int(i))
+            cur=cur.next
+
+        return dummy.next
